1be42538a5cbbea2763b55ca3277b5b871a36006
BlackV1king <aakashyadav1992000@gmail.com>
2024-03-19 11:35:16 +0530

    Content Demo

--- code/content_Generator.py
+++ code/content_Generator.py
@@ -1,6 +1,7 @@
 import json
 import os
 from openai import OpenAI
+import random
 
 client = OpenAI(
     api_key="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",
@@ -9,40 +10,56 @@ client = OpenAI(
 def read_json_data(filename):
   with open(filename, 'r', encoding='utf-8') as f:
     return json.load(f)
+
 data = read_json_data("data/seed_Content.json")
 
-# read the OCR of slides
-# for key, value in data.items():
-#   glens_ocr_value = value.get("glensOcr")
-#   if glens_ocr_value:
-#     print(f"ID: {key}, glensOcr: {glens_ocr_value}")
+def pptGEN():
+  for i in range(5):
+    # pick any random key from the seed content json file
+    target_id = random.choice(list(data.keys()))
+    ocr_value = data[str(target_id)].get("glensOcr")
+    prompt_data = ocr_value
 
-# Specify the ID you want the glensOcr value for
-target_id = "11111"
-ocr_value = data[target_id].get("glensOcr")
+    prompt = f"""You are an assistant for educational content generation.
 
-prompt_data = ocr_value
+    The user will provide the content for first slide. You have to generate the content for 5 slides based on the given content.
 
-prompt = f"""You are an assistant for educational content generation.
+    First slide content : {prompt_data}
 
-The user requests to generate the content for 5 slides based on the following content for the first slide:
+    Desired hierarchy of JSON is given:
+        {target_id}:[
+            slide_01: [
+                slide_title: ,
+                text:
+            ],
+            slide_02: [
+                slide_title: ,
+                text:
+            ]
+        ]
+    I have provided outline, you have to fill the content for slide_01, slide_02, slide_03, slide_04 and slide_05. Each key value pair and target_id should be in double quotes.
+    Here, {target_id} is ID of ppt, and slide_no is the slide number. slide_title is the title of the slide and text is the content of the slide. Slide 1 will be the content I provided, generate the content for 2 to 5th slide.
+    """
 
-{prompt_data}
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          {"role": "user", "content":prompt}
+        ],
+    )
 
-Please provide the content in JSON format, with ppt_id, slide_id, slide_title and text as the elements."""
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    # prompt=prompt
-    messages=[
-      {"role": "user", "content":prompt}
-    ],
-    # response_format={ "type": "json_object" }
-)
-
-generated_text = response.choices[0].message.content
-jso = json.loads(generated_text)
-
-with open('data/generated_content.json', 'w') as f:
-  json.dump(jso, f, indent=4)
-
+    generated_text = response.choices[0].message.content
+    print(generated_text)
+    jso = json.loads(generated_text)
+    print(jso)
+    
+    if os.path.exists("data/generated_content.json") and os.path.getsize("data/generated_content.json") > 0:
+      with open("data/generated_content.json", 'r') as json_file:
+        existing_data = json.load(json_file)
+    else:
+      existing_data = []
+    existing_data.append(jso)
+    with open('data/generated_content.json', 'w') as f:
+      json.dump(existing_data, f, indent=4)
+      
+pptGEN()
